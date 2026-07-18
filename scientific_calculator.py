1afc08b0795556a9ccbdee8bb12387a2e4aade34
Piyush Patil <piyushpatil7983@gmail.com>
2026-07-18 21:51:51 +0530

    Added scientific calculator with keyboard support and working  math functions

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+import math
 
 root = tk.Tk()
 root.title("Scientific Calculator")
 root.geometry("470x650")
-root.resizable(False, False)
+
 
 display = tk.Entry(
     root,
@@ -13,14 +14,7 @@ display = tk.Entry(
     bd=5
 )
 
-display.grid(
-    row=0,
-    column=0,
-    columnspan=5,
-    padx=10,
-    pady=(20,10),
-    ipady=15
-)
+display.grid(row=0, column=0, columnspan=5, padx=10, pady=(20,10), ipady=15)
 
 message = tk.Label(
     root,
@@ -31,127 +25,186 @@ message = tk.Label(
 
 message.grid(row=1,column=0,columnspan=5)
 
-def one():
-    display.insert(tk.END,"1")
-
-def two():
-    display.insert(tk.END,"2")
-
-def three():
-    display.insert(tk.END,"3")
-
-def four():
-    display.insert(tk.END,"4")
-
-def five():
-    display.insert(tk.END,"5")
-
-def six():
-    display.insert(tk.END,"6")
-
-def seven():
-    display.insert(tk.END,"7")
-
-def eight():
-    display.insert(tk.END,"8")
-
-def nine():
-    display.insert(tk.END,"9")
-
-def zero():
-    display.insert(tk.END,"0")
-
-def plus():
-    display.insert(tk.END,"+")
-
-def minus():
-    display.insert(tk.END,"-")
-
-def multiply():
-    display.insert(tk.END,"*")
-
-def divide():
-    display.insert(tk.END,"/")
-
-def decimal():
-    display.insert(tk.END,".")
-
-def open_bracket():
-    display.insert(tk.END,"(")
-
-def close_bracket():
-    display.insert(tk.END,")")
-
-def square():
-    display.insert(tk.END,"²")
-
-def square_root():
-    display.insert(tk.END,"√")
-
-def logarithm():
-    display.insert(tk.END,"log(")
-
-def natural_log():
-    display.insert(tk.END,"ln(")
-
-def percent():
-    display.insert(tk.END,"%")
-
-def clear():
-    display.delete(0,tk.END)
+def click(value):
+    display.insert(tk.END, value)
     message.config(text="")
 
-def equal():
-    message.config(text="Function Coming Soon")
+def clear():
+    display.delete(0, tk.END)
+    message.config(text="")
 
-tk.Button(root,text="(",font=("Arial",15),width=6,height=2,command=open_bracket).grid(row=2,column=0,padx=3,pady=3)
+def backspace():
+    if display.get():
+        display.delete(len(display.get())-1)
 
-tk.Button(root,text=")",font=("Arial",15),width=6,height=2,command=close_bracket).grid(row=2,column=1,padx=3,pady=3)
+def equal(event=None):
+    try:
+        expression = display.get().replace("%","/100")
+        result = eval(expression)
 
-tk.Button(root,text="√",font=("Arial",15),width=6,height=2,command=square_root).grid(row=2,column=2,padx=3,pady=3)
+        display.delete(0, tk.END)
+        display.insert(0, result)
 
-tk.Button(root,text="x²",font=("Arial",15),width=6,height=2,command=square).grid(row=2,column=3,padx=3,pady=3)
+        message.config(text="")
 
-tk.Button(root,text="C",font=("Arial",15),width=6,height=2,command=clear).grid(row=2,column=4,padx=3,pady=3)
+    except:
+        message.config(text="Invalid Input")
 
-tk.Button(root,text="log",font=("Arial",15),width=6,height=2,command=logarithm).grid(row=3,column=0,padx=3,pady=3)
+def square_root():
+    try:
+        number = float(display.get())
 
-tk.Button(root,text="ln",font=("Arial",15),width=6,height=2,command=natural_log).grid(row=3,column=1,padx=3,pady=3)
+        result = math.sqrt(number)
 
-tk.Button(root,text="%",font=("Arial",15),width=6,height=2,command=percent).grid(row=3,column=2,padx=3,pady=3)
+        display.delete(0, tk.END)
+        display.insert(0, result)
 
-tk.Button(root,text="/",font=("Arial",15),width=6,height=2,command=divide).grid(row=3,column=3,padx=3,pady=3)
+        message.config(text="")
 
-tk.Button(root,text="*",font=("Arial",15),width=6,height=2,command=multiply).grid(row=3,column=4,padx=3,pady=3)
+    except:
+        message.config(text="Invalid Input")
 
-tk.Button(root,text="7",font=("Arial",15),width=6,height=2,command=seven).grid(row=4,column=0,padx=3,pady=3)
+def square():
+    try:
+        number = float(display.get())
 
-tk.Button(root,text="8",font=("Arial",15),width=6,height=2,command=eight).grid(row=4,column=1,padx=3,pady=3)
+        result = number ** 2
 
-tk.Button(root,text="9",font=("Arial",15),width=6,height=2,command=nine).grid(row=4,column=2,padx=3,pady=3)
+        display.delete(0, tk.END)
+        display.insert(0, result)
 
-tk.Button(root,text="-",font=("Arial",15),width=6,height=2,command=minus).grid(row=4,column=3,padx=3,pady=3)
+        message.config(text="")
 
-tk.Button(root,text="4",font=("Arial",15),width=6,height=2,command=four).grid(row=5,column=0,padx=3,pady=3)
+    except:
+        message.config(text="Invalid Input")
 
-tk.Button(root,text="5",font=("Arial",15),width=6,height=2,command=five).grid(row=5,column=1,padx=3,pady=3)
+def logarithm():
+    try:
+        number = float(display.get())
 
-tk.Button(root,text="6",font=("Arial",15),width=6,height=2,command=six).grid(row=5,column=2,padx=3,pady=3)
+        result = math.log10(number)
 
-tk.Button(root,text="+",font=("Arial",15),width=6,height=2,command=plus).grid(row=5,column=3,padx=3,pady=3)
+        display.delete(0, tk.END)
+        display.insert(0, result)
 
-tk.Button(root,text="1",font=("Arial",15),width=6,height=2,command=one).grid(row=6,column=0,padx=3,pady=3)
+        message.config(text="")
 
-tk.Button(root,text="2",font=("Arial",15),width=6,height=2,command=two).grid(row=6,column=1,padx=3,pady=3)
+    except:
+        message.config(text="Invalid Input")
 
-tk.Button(root,text="3",font=("Arial",15),width=6,height=2,command=three).grid(row=6,column=2,padx=3,pady=3)
+def natural_log():
+    try:
+        number = float(display.get())
 
-tk.Button(root,text="=",font=("Arial",15),width=6,height=5,command=equal).grid(row=6,column=4,rowspan=2,padx=3,pady=3)
+        result = math.log(number)
 
-tk.Button(root,text="0",font=("Arial",15),width=13,height=2,command=zero).grid(row=7,column=0,columnspan=2,padx=3,pady=3)
+        display.delete(0, tk.END)
+        display.insert(0, result)
 
-tk.Button(root,text=".",font=("Arial",15),width=6,height=2,command=decimal).grid(row=7,column=2,padx=3,pady=3)
+        message.config(text="")
 
-tk.Button(root,text="+/-",font=("Arial",15),width=6,height=2).grid(row=7,column=3,padx=3,pady=3)
+    except:
+        message.config(text="Invalid Input")
+
+def key_pressed(event):
+
+    key = event.char
+
+    allowed = "0123456789+-*/().%"
+
+    if key in allowed:
+        click(key)
+
+root.bind("<Return>", equal)
+root.bind("<BackSpace>", lambda event: backspace())
+root.bind("<Key>", key_pressed)
+
+buttons = [
+    ("(",2,0),
+    (")",2,1),
+    ("√",2,2),
+    ("x²",2,3),
+    ("C",2,4),
+
+    ("log",3,0),
+    ("ln",3,1),
+    ("%",3,2),
+    ("/",3,3),
+    ("*",3,4),
+
+    ("7",4,0),
+    ("8",4,1),
+    ("9",4,2),
+    ("-",4,3),
+
+    ("4",5,0),
+    ("5",5,1),
+    ("6",5,2),
+    ("+",5,3),
+
+    ("1",6,0),
+    ("2",6,1),
+    ("3",6,2),
+
+    ("0",7,0),
+    (".",7,2),
+    ("⌫",7,3)
+]
+
+for text,row,column in buttons:
+
+    if text == "C":
+        command = clear
+
+    elif text == "√":
+        command = square_root
+
+    elif text == "x²":
+        command = square
+
+    elif text == "log":
+        command = logarithm
+
+    elif text == "ln":
+        command = natural_log
+
+    elif text == "⌫":
+        command = backspace
+
+    else:
+        command = lambda t=text: click(t)
+
+    width = 13 if text == "0" else 6
+    columnspan = 2 if text == "0" else 1
+
+    tk.Button(
+        root,
+        text=text,
+        font=("Arial",15),
+        width=width,
+        height=2,
+        command=command
+    ).grid(
+        row=row,
+        column=column,
+        columnspan=columnspan,
+        padx=3,
+        pady=3
+    )
+
+tk.Button(
+    root,
+    text="=",
+    font=("Arial",15),
+    width=6,
+    height=5,
+    command=equal
+).grid(
+    row=6,
+    column=4,
+    rowspan=2,
+    padx=3,
+    pady=3
+)
 
 root.mainloop()
